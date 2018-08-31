b3747b921c1b8a09739f97aab26efc90596e886e
r_cohen <rogeliocohen11@gmail.com>
2018-08-31 18:57:29 -0500

    Pruebita

--- src/hola_ltcr/Hola_LTCR.java
+++ src/hola_ltcr/Hola_LTCR.java
@@ -11,11 +11,9 @@ package hola_ltcr;
  */
 public class Hola_LTCR {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("LTCR TE AMO ♥");
     }
     
 }
